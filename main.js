a237a2797d61a75a5e396e5acadb9a7df6d13461
Vincent Pastorino <vpastori@DESKTOP-TDG3FUD>
2023-04-27 19:58:23 -0400

    finished lab

--- main.js
+++ main.js
@@ -4,7 +4,38 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const errorModal = document.querySelector('#modal');
+const modalMessage = document.querySelector('#modal-message');
+const likeButtons = document.querySelectorAll('.like-glyph');
 
+document.addEventListener("DOMContentLoaded", () => {
+
+  document.querySelector("#modal").classList.add("hidden");
+
+  const likeButtons = document.querySelectorAll(".like-glyph");
+  for (let button of likeButtons) {
+    button.addEventListener("click", () => {
+      mimicServerCall()
+        .then(() => {
+          if (button.classList.contains("activated-heart")) {
+            button.innerHTML = EMPTY_HEART;
+            button.classList.remove("activated-heart");
+          } else {
+            button.innerHTML = FULL_HEART;
+            button.classList.add("activated-heart");
+          }
+        })
+        .catch(error => {
+          document.querySelector("#modal-message").innerHTML = error;
+          document.querySelector("#modal").classList.remove("hidden");
+          setTimeout(() => {
+            document.querySelector("#modal").classList.add("hidden");
+          }, 3000);
+        });
+    });
+  }
+
+});
 
 
 //------------------------------------------------------------------------------
